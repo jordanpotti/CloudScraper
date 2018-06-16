129d1069e6f4fe4638620c9d12ea659aa06214e9
Unknown <fherreraf00@gmail.com>
2018-06-16 12:32:46 +0200

    Initial commit of the fork
    Changed BeautifulSoup and re expresions for requests_html to gather links.

--- CloudScraper.py
+++ CloudScraper.py
@@ -1,12 +1,9 @@
-import requests
-import re
+from requests_html import HTMLSession
 from argparse import ArgumentParser
+from rfc3987 import parse
+import requests
+import time
 import sys
-from bs4 import BeautifulSoup
-
-grep_list = None
-
-arguments = None
 
 def print_banner():
         print('''\nDescription: 
@@ -18,112 +15,95 @@ def print_banner():
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
           }
 
+
 def start(target, depth):
-    print("Beginning search for cloud resources in ", target, "\n")
+    s = HTMLSession()
+
+    print("Beginning search for cloud resources in {}".format(target))
+    html = s.get(target, allow_redirects=True, headers=headers).html
+    
+    links = list(html.absolute_links)
+    print('Initial links: {}\n'.format(len(links)))
+
+    spider(links, target, depth, s)
+    
+def worker(target_, depth, url, s):
     try:
-        start_page = requests.get(target,allow_redirects=True,headers=headers)
-    except requests.exceptions.RequestException as e:
-        if 'https' in target:
-            try:
-                start_page = requests.get(target.replace('https','http'),allow_redirects=True,headers=headers)
-            except requests.exceptions.RequestException as e:
-                print(e)
-    links = []
-    soup = BeautifulSoup(start_page.text, "lxml")
-    for link in soup.findAll('a', attrs={'href':re.compile("^http://")}):
-        links.append(link.get('href'))
-    for link in soup.findAll('a', attrs={'href':re.compile("^https://")}): 
-        links.append(link.get('href'))
-    for link in soup.findAll('link', attrs={'href':re.compile("^http://")}): 
-        links.append(link.get('href'))
-    for link in soup.findAll('link', attrs={'href':re.compile("^https://")}): 
-        links.append(link.get('href'))
+        if (target_['authority'] in parse(url)['authority']) and url.count("/") < depth+2:
+            html = s.get(url, allow_redirects=True, headers=headers).html
+            links = html.absolute_links
+                
+            if arguments.v:
+                print('{} links found [{}]'.format(len(links), url))
+                
+            return links
 
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',start_page.text)
-    links.extend(urls)
-    spider(links,target,depth)
+    except ValueError:
+        pass
+        
 
-def spider(links,target,depth):
-    target_clean = target.replace("https://","")
-    target_clean_2 = target_clean.replace("http://","")
+def spider(links, target, depth, s):
+    target_ = parse(target)
+
     for url in links:
-        if (target_clean_2 in url) and url.count("/") < depth+2:
-            try:
-                page = requests.get(url, allow_redirects=True, headers=headers)
-                soup = BeautifulSoup(page.text, "lxml")
-                for link in soup.findAll('a', attrs={'href':re.compile("^http://")}):
-                    links.append(link.get('href'))
-                for link in soup.findAll('a', attrs={'href':re.compile("^https://")}): 
-                    links.append(link.get('href'))
-                for link in soup.findAll('link', attrs={'href':re.compile("^http://")}): 
-                    links.append(link.get('href'))
-                for link in soup.findAll('link', attrs={'href':re.compile("^https://")}): 
-                    links.append(link.get('href'))
-            except requests.exceptions.RequestException as e:  # This is the correct syntax
-                if 'https' in target: 
-                    try:
-                        page = requests.get(target.replace('https','http'),allow_redirects=True,headers=headers)
-                        soup = BeautifulSoup(page.text, "lxml")
-                        for link in soup.findAll('a', attrs={'href':re.compile("^http://")}):
-                            links.append(link.get('href'))
-                        for link in soup.findAll('a', attrs={'href':re.compile("^https://")}): 
-                            links.append(link.get('href'))
-                        for link in soup.findAll('link', attrs={'href':re.compile("^http://")}): 
-                            links.append(link.get('href'))
-                        for link in soup.findAll('link', attrs={'href':re.compile("^https://")}): 
-                            links.append(link.get('href'))
-                    except requests.exceptions.RequestException as e:
-                        print(e)
-            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',page.text)
-            links.extend(urls)
-        links = list(set(links))
+        old_len = len(links)
+        new_links = worker(target_, depth, url, s)
+        
+        try:
+            [links.append(link) for link in new_links if link not in links]
+            print('Added links: {} [{}]\n'.format(len(links)-old_len, len(links)))
+        except TypeError:
+            pass
+
     parser(links)
 
 
 def parser(links):
+    print('Total links: ', len(links))
     matches = []
-    cloud_matches=['amazonaws.com','digitaloceanspaces.com','windows.net']
-    for strings in cloud_matches:
-        for link in links:
-            if strings in link:
-                matches.append(link)
+    cloud_domains = ['amazonaws.com', 'digitaloceanspaces.com', 'windows.net']
+
+    [[matches.append(link) for link in links if cloud_domain in link] for cloud_domain in cloud_domains]
     matches = list(set(matches))
+    
     if len(matches) == 0:
         print("There were no matches!")
+    
     else:
-        print("\nThere were ", len(matches), " matches for this search!")
+        print("\nThere were {} matches for this search!".format(len(matches)))
         for match in matches:
             print(match, "\n")
 
-def main():
-    global arguments
-    global grep_list
+def args():
     parser = ArgumentParser()
     parser.add_argument("-u", dest="URL", required=False, help="Target Scope") 
-    parser.add_argument("-d", dest="depth",type=int, required=False, default=25, help="Max Depth of links Default: 25")
+    parser.add_argument("-d", dest="depth", type=int, required=False, default=25, help="Max Depth of links Default: 25")
     parser.add_argument("-l", dest="targetlist", required=False, help="Location of text file of Line Delimited targets") 
+    parser.add_argument("-v", action="store_true", default=False, required=False, help="Verbose output")
 
     if len(sys.argv) == 1:
         parser.error("No arguments given.")
         parser.print_usage
         sys.exit()
 
-#ouput parsed arguments into a usable object
-    arguments = parser.parse_args()
+    #ouput parsed arguments into a usable object
+    return parser.parse_args()
 
+def cleaner(url):
+    if 'http' not in url:
+        return ("https://"+url).rstrip()
+    else:
+        return url
+
+def main():
     if arguments.targetlist:
         with open (arguments.targetlist, 'r') as target_list:
-            for line in target_list:
-                if 'http' not in line:
-                    line_mod = "https://"+line
-                    start(line_mod.rstrip(), arguments.depth)
-                else:
-                    start(line, arguments.depth)
-    else:                
-        if 'http' not in arguments.URL:
-            line_mod = "https://"+arguments.URL
-            start(line_mod.rstrip(), arguments.depth)
-        else:
-            start(arguments.URL, arguments.depth)
+            [start(cleaner(line), arguments.depth) for line in target_list]
+    else:
+        start(cleaner(arguments.URL), arguments.depth)
+
+global arguments
+arguments = args()
+
 print_banner()
 main()
